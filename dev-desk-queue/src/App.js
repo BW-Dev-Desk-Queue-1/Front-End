dba8b93ac4b5006fb8ea54973cd24e557b96b7ff
Daniel <fireblastdaniel@yahoo.com>
2020-02-04 10:24:02 -0800

    final commit for merge

--- dev-desk-queue/src/App.js
+++ dev-desk-queue/src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-<<<<<<< HEAD
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import StudentDashboard from './components/Student/StudentDashboard.js';
 import HelperDashboard from './components/Helper/HelperDashboard.js';
 import CreateTicket from './components/CreateTicket/CreateTicket.js';
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Route exact path='/student' component={StudentDashboard} />
-        <Route exact path='/ticket' component={CreateTicket} />
-        <Route exact path='/helper' component={HelperDashboard} /> 
-=======
-import { BrowserRouter as Router, Route, Switch  } from 'react-router-dom';
-import './App.css';
+
 
 import Login from "./components/Login"
-import Dashboard from "./components/Dashboard"
 import PrivateRoute from "./utils/PrivateRoute";
 
 import Register from './components/Register';
@@ -29,14 +18,12 @@ function App() {
   return (
     <Router>    
       <div className="App">
-        <NavTab />
         <Register />
-      <Switch>
+      <Switch> 
         <Route exact path='/' component={Login}/>
         <Route path="/home" component={Login} />
-        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/dashboard" component={StudentDashboard} component={HelperDashboard} />
       </Switch>
->>>>>>> 96a1d84d49b3047098fd3164fdd9cecb2baa18a9
       </div>
     </Router>
   );
