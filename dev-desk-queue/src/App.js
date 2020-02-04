9ee6d2195920bc104bca5e3e8c1a2016da6208e0
Daniel <fireblastdaniel@yahoo.com>
2020-02-04 09:33:36 -0800

    getting ready to merge

--- dev-desk-queue/src/App.js
+++ dev-desk-queue/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import StudentDashboard from './components/Student/StudentDashboard.js';
 import HelperDashboard from './components/Helper/HelperDashboard.js';
+import CreateTicket from './components/CreateTicket/CreateTicket.js';
 
 function App() {
   return (
-    <div className="App">
-       <Route path='/student' component={StudentDashboard} />
-       <Route Path='/helper' component={HelperDashboard} /> 
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path='/student' component={StudentDashboard} />
+        <Route exact path='/ticket' component={CreateTicket} />
+        <Route exact path='/helper' component={HelperDashboard} /> 
+      </div>
+    </Router>
   );
 }
 
