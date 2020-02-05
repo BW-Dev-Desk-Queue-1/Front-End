99d25b01c436b6e018c0b68791a7d53bef8a9d5c
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-02-05 09:58:40 -0800

    preparing for merge

--- dev-desk-queue/src/components/CreateTicket/CreateTicket.js
+++ dev-desk-queue/src/components/CreateTicket/CreateTicket.js
@@ -17,8 +17,10 @@ return (
   <div className='modal'>
     <div className='ticket'>
       <span className='close' onClick={props.closeTicket}>&times;</span>
+      <h2>Let's submit a help ticket</h2>
+      <p><span className='red'>*</span> Required Fields</p>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label>Title</label>
+        <label><span className='red'>*</span> What's going on?</label>
         <input
           name="title"
           defaultValue=""
@@ -26,16 +28,22 @@ return (
         />
         {errors.title && <p className="error">Title is required</p>}
         <br />
-        <label>Description</label>
-        <input name="description" ref={register({ required: true })} />
-        {errors.description && <p className="error">Description is required</p>}
-        <br />
-        <label>Category</label>
-        <input name="category" ref={register({ required: true })} />
+        <label><span className='red'>*</span> What is this issue about?</label>
+        <select name="category" ref={register({ required: true })}>
+          <option value='Equipment'>Equipment</option>
+          <option value='People'>People</option>
+          <option value='Track'>Track</option>
+          <option value='Finances'>Finances</option>
+          <option value='Other'>Other</option>
+        </select>
         {errors.category && <p className="error">Description is required</p>}
         <br />
+        <label>Description</label>
+        <textarea name="description" rows='5' cols='50' ref={register({ required: true })} />
+        {errors.description && <p className="error">Description is required</p>}
+        <br />
         <label>What have you Tried?</label>
-        <input name="tried" ref={register({ required: true })} />
+        <textarea name="tried" ref={register({ required: true })} />
         {errors.tried && <p className="error">Tell us What you have Tried</p>}
         <br />
         <input type="submit" />
